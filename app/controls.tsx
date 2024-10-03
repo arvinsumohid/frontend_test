7932246c97884c9632d976f37771d073b6ea8f02
arvinsumohid <arvinsumohid99@gmail.com>
2024-10-04 00:33:18 +0800

    [Medium][Feature] Sort users solution

--- app/controls.tsx
+++ app/controls.tsx
@@ -1,6 +1,10 @@
-import Select from "react-select";
+import { useCallback, useEffect, useState } from "react";
+import Select, { SingleValue } from "react-select";
+import { User } from "./types/user";
 
-const Controls = () => {
+const Controls = ({ handleSort }: { handleSort: (name: keyof User, type: string) => void }) => {
+  const [selectedField, setSelectedField] = useState<string>('name');
+  const [selectedDirection, setSelectedDirection] = useState<string>('ascending');
   const fieldOptions = [
     { label: "Name", value: "name" },
     { label: "Company", value: "company" },
@@ -11,22 +15,47 @@ const Controls = () => {
     { label: "Descending", value: "descending" },
   ];
 
+  useEffect(() => {
+    handleSort(selectedField as keyof User, selectedDirection);
+  }, [selectedField, selectedDirection]);
+
+
+  const handleSelectField = (e: SingleValue<{ label: string, value: string }>) => {
+    if (!e) return;
+    setSelectedField(e.value);
+  }
+
+  const handleSelectDirection = (e: SingleValue<{ label: string, value: string }>) => {
+    if (!e) return;
+    setSelectedDirection(e.value);
+  }
+
   return (
     <div className="gallery-controls controls">
       <div className="form-group group">
         <label htmlFor="sort-field" className="label">
           Sort Field
         </label>
-        <Select options={fieldOptions} inputId="sort-field" className="input" />
+        <Select
+          id="sort-field"
+          defaultValue={fieldOptions[0]}
+          options={fieldOptions}
+          inputId="sort-field"
+          className="input"
+          onChange={handleSelectField}
+        />
       </div>
       <div className="form-group group">
         <label htmlFor="sort-direction" className="label">
           Sort Direction
         </label>
         <Select
+          id="sort-direction"
+          defaultValue={directionOptions[0]}
           options={directionOptions}
           inputId="sort-direction"
           className="input"
+          onChange={handleSelectDirection}
         />
       </div>
     </div>
